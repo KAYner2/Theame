6791948e76cb0424227e5ae585473d1f9db84941
KAYner2 <pastuxovkirill216@gmail.com>
2025-09-19 23:06:23 +0500

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -118,7 +118,7 @@ export default {
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			},
 			fontFamily: {
-				'forum': ['Forum', 'serif'],
+				montserrat: ['Montserrat', 'ui-sans-serif', 'system-ui', 'Arial', 'sans-serif'],
 			}
 		}
 	},
